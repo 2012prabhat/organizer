33a07f8bae26528a103a54d1b30adcb87c0e1e11
Prabhat Kumar <83307497+2012prabhat@users.noreply.github.com>
2022-01-04 17:07:40 +0530

    Delete help.js

--- command/help.js
+++
@@ -1,9 +0,0 @@
-function helpFn() {
-    console.log(`list of all the commands
-                     1. node main.js tree "path"
-                     2. node main.js organize "path"
-                     3. node main.js help`)
- }
- module.exports = {
-     helpfxn: helpFn
- }
